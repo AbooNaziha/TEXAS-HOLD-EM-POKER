c6e4761005639c4ff2bc22816064e01bf0b94d3a
Aboo Naziha <66263275+Muni-Ismail@users.noreply.github.com>
2021-02-04 10:18:27 +0100

    Texas Hold'em poker_Ismail

--- poker/hand.py
+++ poker/hand.py
@@ -3,4 +3,33 @@ class Hand():
         self.cards  = cards
  
     def best_rank(self):
+        ranks_with_three_of_a_kind = self._ranks_with_count(3)
+
+        if len(ranks_with_three_of_a_kind) == 1:
+            return "Three of a Kind"
+ 
+        ranks_with_pairs = self._ranks_with_count(2)
+       
+        if len(ranks_with_pairs) == 2:
+            return "Two Pair"
+
+        if len(ranks_with_pairs) == 1:
+            return "Pair"
+
         return "High Card"
+
+    def _ranks_with_count(self, count):
+        return {
+            rank: rank_count
+            for rank, rank_count in self._card_rank_count.items()
+            if rank_count == count
+        }
+
+
+    @property
+    def _card_rank_count(self):
+        card_rank_count = {}
+        for card in self.cards:
+            card_rank_count.setdefault(card.rank, 0)
+            card_rank_count[card.rank] += 1
+        return card_rank_count
--- test/test_hand.py
+++ test/test_hand.py
@@ -28,3 +28,49 @@ class HandTest(unittest.TestCase):
             hand.best_rank(), 
             "High Card"
         )
+
+    def test_figure_out_pair_is_best_rank(self):
+        cards = [
+            Card(rank= "Ace", suit= "Spades"),
+            Card(rank= "Ace", suit= "Clubs")
+        ]
+
+        hand = Hand(cards = cards)
+
+        self.assertEqual(
+            hand.best_rank(),
+            "Pair"
+        )
+    def test_figure_out_two_pair_is_best_rank(self):
+        cards = [
+            Card(rank = "Ace", suit = "Spades"),
+            Card(rank = "5", suit = "Clubs"),
+            Card(rank = "Ace", suit = "Clubs"),
+            Card(rank = "King", suit = "Hearts"),
+            Card(rank = "King", suit = "Diamonds")
+        ]
+
+        hand = Hand(cards = cards)
+
+        self.assertEqual(
+            hand.best_rank(),
+            "Two Pair"
+        )
+    
+    def test_figure_out_three_of_a_kind_is_best_rank(self):
+        cards = [
+            Card(rank = "King", suit = "Clubs"),
+            Card(rank = "King", suit = "Hearts"),
+            Card(rank = "King", suit = "Diamonds"),
+            Card(rank = "Ace", suit = "Spades"),
+            Card(rank = "5", suit = "Clubs")
+        ]
+
+        hand = Hand(cards = cards)
+
+        self.assertEqual(
+            hand.best_rank(),
+            "Three of a Kind"
+        )
+    
+    
